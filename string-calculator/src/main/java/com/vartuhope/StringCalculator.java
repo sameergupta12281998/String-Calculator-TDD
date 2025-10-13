ec9b18bafc60fb731b0461fc0795d7ff05b2bdda
sameergupta12281998 <sameergupta122898@gmail.com>
2025-10-13 22:43:28 +0530

    feat: support newline as separator (green)

--- string-calculator/src/main/java/com/vartuhope/StringCalculator.java
+++ string-calculator/src/main/java/com/vartuhope/StringCalculator.java
@@ -2,17 +2,18 @@ package com.vartuhope;
 
 public class StringCalculator {
 
-    // Adds numbers in a string, separated by commas or newlines, multiple Numbers allowed
+    // Adds numbers in a string, 
+    //separated by commas or newlines,
+    // multiple Numbers allowed
+
     public int add(String numbers) {
         if (numbers == null || numbers.isEmpty()) return 0;
-
 
         if (!numbers.contains(",") && !numbers.contains("\n")) {
             return Integer.parseInt(numbers);
         }
 
-        // split on comma for now
-        String[] tokens = numbers.split(",");
+        String[] tokens = numbers.split(",|\n");
         int sum = 0;
         for (String t : tokens) {
             if (t.isEmpty()) continue;
