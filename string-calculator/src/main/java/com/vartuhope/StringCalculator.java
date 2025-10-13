9c1f44e4d8e97198f203d89bb1864db568cc9faa
sameergupta12281998 <sameergupta122898@gmail.com>
2025-10-13 22:17:59 +0530

    feat: support single number input (green)

--- string-calculator/src/main/java/com/vartuhope/StringCalculator.java
+++ string-calculator/src/main/java/com/vartuhope/StringCalculator.java
@@ -1,5 +1,18 @@
 package com.vartuhope;
 
 public class StringCalculator {
-    
+    public int add(String numbers) {
+        if (numbers == null || numbers.isEmpty()) return 0;
+
+
+        if (!numbers.contains(",") && !numbers.contains("\n")) {
+            try {
+                return Integer.parseInt(numbers);
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("Invalid number", e);
+            }
+        }
+
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
 }
