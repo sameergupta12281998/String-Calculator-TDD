9aefdd42684bf2c789b6c8c9ac957c4a853ff357
sameergupta12281998 <sameergupta122898@gmail.com>
2025-10-13 23:53:33 +0530

    feat: throw when negative numbers present (list all negatives) (green)

--- string-calculator/src/main/java/com/vartuhope/StringCalculator.java
+++ string-calculator/src/main/java/com/vartuhope/StringCalculator.java
@@ -1,5 +1,7 @@
 package com.vartuhope;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -8,16 +10,31 @@ public class StringCalculator {
     public int add(String numbers) {
         if (numbers == null || numbers.isEmpty()) return 0;
 
-        Pattern pattern = Pattern.compile("\\d+");
+        Pattern pattern = Pattern.compile("-?\\d+");
         Matcher matcher = pattern.matcher(numbers);
 
         int sum = 0;
+        String negatives = "";
         while (matcher.find()) {
             String numStr = matcher.group();
             int n = Integer.parseInt(numStr);
-            sum += n;
+            if (n < 0) {
+                negatives += (negatives.isEmpty() ? "" : ",") + n;
+            } else {
+                sum += n;
+            }
+        }
+
+        if (!negatives.isEmpty()) {
+            throw new NegativeNumberException("negative numbers not allowed : " + negatives);
         }
 
         return sum;
+    }
+
+
+
+    public static class NegativeNumberException extends IllegalArgumentException {
+        public NegativeNumberException(String message) { super(message); }
     }
 }
